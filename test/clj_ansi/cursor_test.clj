34b6a83cd3af674bdc8292d43cd7a2f77ed10307
Pedro Matiello <pedro@pmatiello.me>
2020-07-20 16:06:49 -0300

    Add deftest for every function in sgr and cursor namespaces.

--- test/clj_ansi/cursor_test.clj
+++ test/clj_ansi/cursor_test.clj
@@ -1,10 +1,25 @@
 (ns clj-ansi.cursor-test
   (:require [clojure.test :refer :all]
-            [clojure.string :as str]
-            [clj-ansi.cursor]))
+            [clj-ansi.cursor :as cursor]))
 
-(deftest cursor-test
-  (doseq [each (-> 'clj-ansi.cursor ns-publics keys)]
-    (testing (str each " starts with CSI")
-      (let [fn (->> each str (symbol "clj-ansi.cursor") resolve var-get)]
-        (is (true? (str/starts-with? (fn 1) "\u001b[")))))))
+(deftest cursor-up-test
+  (is (= "\u001b[1A") (cursor/cursor-up 1)))
+
+(deftest cursor-down-test
+  (is (= "\u001b[1B") (cursor/cursor-down 1)))
+
+(deftest cursor-forward-test
+  (is (= "\u001b[1C") (cursor/cursor-forward 1)))
+
+(deftest cursor-back-test
+  (is (= "\u001b[1D") (cursor/cursor-back 1)))
+
+(deftest cursor-next-test
+  (is (= "\u001b[1E") (cursor/cursor-next-line 1)))
+
+(deftest cursor-previous-test
+  (is (= "\u001b[1F") (cursor/cursor-previous-line 1)))
+
+(deftest cursor-column-test
+  (is (= "\u001b[1G") (cursor/cursor-column 1)))
+

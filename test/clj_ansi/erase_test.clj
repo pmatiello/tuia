cabf7318f223b934ec21fdb48b5af433f16f2f35
Pedro Matiello <pedro@pmatiello.me>
2020-07-25 21:28:58 -0300

    Rename functions the erase namespace.

--- test/clj_ansi/erase_test.clj
+++ test/clj_ansi/erase_test.clj
@@ -2,11 +2,11 @@
   (:require [clojure.test :refer :all]
             [clj-ansi.erase :as erase]))
 
-(deftest from-cursor-test
-  (is (= "\u001b[0J" erase/from-cursor)))
+(deftest below-test
+  (is (= "\u001b[0J" erase/below)))
 
-(deftest to-cursor-test
-  (is (= "\u001b[1J" erase/to-cursor)))
+(deftest above-test
+  (is (= "\u001b[1J" erase/above)))
 
-(deftest full-screen-test
-  (is (= "\u001b[2J" erase/full-screen)))
+(deftest all-test
+  (is (= "\u001b[2J" erase/all)))

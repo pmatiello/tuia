9662a7501933cf82885abde7508b72595410a235
Pedro Matiello <pedro@pmatiello.me>
2020-07-25 21:03:17 -0300

    Rename functions in cursor namespace.

--- demo/clj_ansi/cursor_demo.clj
+++ demo/clj_ansi/cursor_demo.clj
@@ -3,12 +3,12 @@
 
 (defn -main []
   (print "First line")
-  (print (cursor/cursor-next-line 2))
+  (print (cursor/next-line 2))
   (print "Two lines from the first")
-  (print (cursor/cursor-previous-line 1))
+  (print (cursor/previous-line 1))
   (print "Up one line from below")
-  (print (cursor/cursor-next-line 2))
+  (print (cursor/next-line 2))
   (print "This will be overwritten")
-  (print (cursor/cursor-column 1))
+  (print (cursor/column 1))
   (print "This overwrittes something")
   (println))

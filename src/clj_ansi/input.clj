4727ffa271524ea2388fc308683135c1e56cf507
Pedro Matiello <pedro@pmatiello.me>
2020-09-21 23:59:51 -0300

    Add reader->event-seq function to produce events from an input reader.

--- src/clj_ansi/input.clj
+++ src/clj_ansi/input.clj
@@ -1,44 +1,8 @@
 (ns clj-ansi.input
-  (:require [clj-ansi.internal.input-event :as input-event])
+  (:require [clj-ansi.internal.input-parsing :as input-parsing])
   (:import (java.io Reader)))
 
-(defn ^:private each->key [acc key]
-  (cond
-    (and (:has-next? key) (= (:char-code key) 27))
-    (let [result @acc]
-      (vreset! acc [key])
-      result)
-
-    (:has-next? key)
-    (do (vswap! acc conj key)
-        nil)
-
-    :else
-    (let [result (conj @acc key)]
-      (vreset! acc [])
-      result)))
-
-(defn ^:private with-grouped-escape-seqs [input-seq]
-  (let [acc (volatile! [])]
-    (->> input-seq
-         (map (partial each->key acc))
-         (remove empty?))))
-
-(def ^:private key->key-codes
-  (partial map :char-code))
-
-(defn input-seq->event-seq [input-seq]
-  (->> input-seq
-       with-grouped-escape-seqs
-       (map key->key-codes)
-       (map input-event/key-codes->event)
-       (remove #{::input-event/omit})))
-
-(defn reader->input-seq [^Reader reader]
-  (lazy-seq
-    (let [char-code (.read reader)
-          _         (if (= 27 char-code)
-                      (Thread/sleep 10))
-          has-next? (.ready reader)]
-      (cons {:char-code char-code :has-next? has-next?}
-            (reader->input-seq reader)))))
+(defn reader->event-seq [^Reader reader]
+  (->> reader
+       input-parsing/reader->input-seq
+       input-parsing/input-seq->event-seq))
--- test/clj_ansi/input_test.clj
+++ test/clj_ansi/input_test.clj
@@ -3,51 +3,10 @@
             [clj-ansi.input :as input])
   (:import (java.io StringReader)))
 
-(deftest input-seq->event-seq-test
-  (testing "decodes regular characters"
-    (is (= [{:event :keypress :value "A"}
-            {:event :keypress :value "B"}
-            {:event :keypress :value "C"}]
-           (input/input-seq->event-seq [{:char-code 65 :has-next? false}
-                                        {:char-code 66 :has-next? false}
-                                        {:char-code 67 :has-next? false}]))))
-
-  (testing "decodes control characters"
-    (is (= [{:event :keypress :value :nul}]
-           (input/input-seq->event-seq [{:char-code 0 :has-next? false}]))))
-
-  (testing "decodes escape sequences"
-    (is (= [{:event :keypress :value :up}]
-           (input/input-seq->event-seq [{:char-code 27, :has-next? true}
-                                        {:char-code 91, :has-next? true}
-                                        {:char-code 65, :has-next? false}]))))
-
-  (testing "returns :unknown when escape sequence is not recognised"
-    (is (= [{:event :unknown}]
-           (input/input-seq->event-seq [{:char-code 27, :has-next? true}
-                                        {:char-code 99, :has-next? true}
-                                        {:char-code 99, :has-next? false}]))))
-
-  (testing "splits multiple buffered escape sequences"
-    (is (= [{:event :keypress :value :up}
-            {:event :keypress :value :esc}
-            {:event :keypress :value :down}]
-           (input/input-seq->event-seq [{:char-code 27, :has-next? true}
-                                        {:char-code 91, :has-next? true}
-                                        {:char-code 65, :has-next? true}
-                                        {:char-code 27, :has-next? true}
-                                        {:char-code 27, :has-next? true}
-                                        {:char-code 91, :has-next? true}
-                                        {:char-code 66, :has-next? false}])))))
-
-(deftest reader->input-seq-test
-  (let [test-string "input"
-        reader      (StringReader. test-string)]
-    (is (= [{:char-code 105, :has-next? true}
-            {:char-code 110, :has-next? true}
-            {:char-code 112, :has-next? true}
-            {:char-code 117, :has-next? true}
-            {:char-code 116, :has-next? true}]
-           (->> reader
-                input/reader->input-seq
-                (take (count test-string)))))))
+(deftest reader->event-seq-test
+  (is (= [{:event :keypress :value "i"}
+          {:event :keypress :value "n"}
+          {:event :keypress :value "p"}
+          {:event :keypress :value "u"}
+          {:event :keypress :value "t"}]
+         (-> "input" StringReader. input/reader->event-seq))))

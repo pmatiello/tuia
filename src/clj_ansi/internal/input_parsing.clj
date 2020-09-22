a9a76fd9f23b8864291da778c1e52980d5da9929
Pedro Matiello <pedro@pmatiello.me>
2020-09-22 00:13:20 -0300

    Extract functions and constants in internal.input-parsing.

--- src/clj_ansi/internal/input_parsing.clj
+++ src/clj_ansi/internal/input_parsing.clj
@@ -2,14 +2,22 @@
   (:require [clj-ansi.internal.input-event :as input-event])
   (:import (java.io Reader)))
 
+(defn ^:private starting-escape-sequence? [key]
+  (and (:has-next? key) (= (:char-code key) 27)))
+
+(defn ^:private continuing-escape-sequence? [acc key]
+  (and (not-empty @acc) (:has-next? key)))
+
+(def ^:private escape-seq-timeout-ms 10)
+
 (defn ^:private each->key [acc key]
   (cond
-    (and (:has-next? key) (= (:char-code key) 27))
+    (starting-escape-sequence? key)
     (let [result @acc]
       (vreset! acc [key])
       result)
 
-    (and (not-empty @acc) (:has-next? key))
+    (continuing-escape-sequence? acc key)
     (do (vswap! acc conj key)
         nil)
 
@@ -37,7 +45,7 @@
   (lazy-seq
     (let [char-code (.read reader)
           _         (if (= 27 char-code)
-                      (Thread/sleep 10))
+                      (Thread/sleep escape-seq-timeout-ms))
           has-next? (.ready reader)]
       (when (not= char-code -1)
         (cons {:char-code char-code :has-next? has-next?}

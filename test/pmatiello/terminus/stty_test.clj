a0ce05406851fd44cf975f684437ea96b9860b9d
Pedro Matiello <pedro@pmatiello.me>
2020-09-22 21:30:10 -0300

    Throw exception when call to stty fails.

--- test/pmatiello/terminus/stty_test.clj
+++ test/pmatiello/terminus/stty_test.clj
@@ -3,29 +3,54 @@
             [mockfn.clj-test :as mfn]
             [mockfn.matchers :as mfn.matchers]
             [pmatiello.terminus.stty :as stty]
-            [clojure.java.shell :refer [sh]]))
+            [clojure.java.shell :refer [sh]])
+  (:import (clojure.lang ExceptionInfo)))
 
 (mfn/deftest current-test
-  (is (= {:gfmt1 nil :cflag "4b00" :iflag "2306"}
-         (stty/current)))
-  (mfn/providing
-    (sh "/bin/sh" "-c" "stty -g < /dev/tty")
-    {:out "gfmt1:cflag=4b00:iflag=2306\n"}))
+  (mfn/testing "returns current line terminal settings"
+    (is (= {:gfmt1 nil :cflag "4b00" :iflag "2306"}
+           (stty/current)))
+    (mfn/providing
+      (sh "/bin/sh" "-c" "stty -g < /dev/tty")
+      {:exit 0 :out "gfmt1:cflag=4b00:iflag=2306\n"}))
+
+  (mfn/testing "throws exception on error"
+    (is (thrown? ExceptionInfo (stty/current)))
+    (mfn/providing
+      (sh "/bin/sh" "-c" "stty -g < /dev/tty") {:exit 1})))
 
 (mfn/deftest apply!-test
-  (stty/apply! {:gfmt1 nil :cflag "4b00" :iflag "2306"})
-  (mfn/verifying
-    (sh "/bin/sh" "-c" "stty gfmt1:cflag=4b00:iflag=2306 < /dev/tty")
-    :irrelevant (mfn.matchers/exactly 1)))
+  (mfn/testing "applies line terminal settings"
+    (stty/apply! {:gfmt1 nil :cflag "4b00" :iflag "2306"})
+    (mfn/verifying
+      (sh "/bin/sh" "-c" "stty gfmt1:cflag=4b00:iflag=2306 < /dev/tty")
+      {:exit 0} (mfn.matchers/exactly 1)))
+
+  (mfn/testing "throws exception on error"
+    (is (thrown? ExceptionInfo (stty/apply! {:gfmt1 nil})))
+    (mfn/providing
+      (sh "/bin/sh" "-c" "stty gfmt1 < /dev/tty") {:exit 2})))
 
 (mfn/deftest set-flags!-test
-  (stty/set-flags! :icanon :echo)
-  (mfn/verifying
-    (sh "/bin/sh" "-c" "stty icanon echo < /dev/tty")
-    :irrelevant (mfn.matchers/exactly 1)))
+  (mfn/testing "enables specific line terminal configuration flags"
+    (stty/set-flags! :icanon :echo)
+    (mfn/verifying
+      (sh "/bin/sh" "-c" "stty icanon echo < /dev/tty")
+      {:exit 0} (mfn.matchers/exactly 1)))
+
+  (mfn/testing "throws exception on error"
+    (is (thrown? ExceptionInfo (stty/set-flags! :icanon)))
+    (mfn/providing
+      (sh "/bin/sh" "-c" "stty icanon < /dev/tty") {:exit 3})))
 
 (mfn/deftest unset-flags!-test
-  (stty/unset-flags! :icanon :echo)
-  (mfn/verifying
-    (sh "/bin/sh" "-c" "stty -icanon -echo < /dev/tty")
-    :irrelevant (mfn.matchers/exactly 1)))
+  (mfn/testing "disables specific line terminal configuration flags"
+    (stty/unset-flags! :icanon :echo)
+    (mfn/verifying
+      (sh "/bin/sh" "-c" "stty -icanon -echo < /dev/tty")
+      {:exit 0} (mfn.matchers/exactly 1)))
+
+  (mfn/testing "throws exception on error"
+    (is (thrown? ExceptionInfo (stty/unset-flags! :echo)))
+    (mfn/providing
+      (sh "/bin/sh" "-c" "stty -echo < /dev/tty") {:exit 4})))

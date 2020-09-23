0875320ba148543237dfbd2f145c641a8db8a3c3
Pedro Matiello <pedro@pmatiello.me>
2020-09-22 21:30:10 -0300

    Throw exception when call to stty fails.

--- src/pmatiello/terminus/stty.clj
+++ src/pmatiello/terminus/stty.clj
@@ -1,13 +1,22 @@
 (ns pmatiello.terminus.stty
-  (:require [clojure.java.shell :refer [sh]]
+  (:require [clojure.java.shell :as shell]
             [clojure.string :as string]))
+
+(defn ^:private sh! [& args]
+  (let [command  (string/join " " args)
+        result   (shell/sh "/bin/sh" "-c" command)
+        success? (-> result :exit zero?)]
+    (when-not success?
+      (throw (ex-info "Shell command execution failed"
+                      {:command command :result result})))
+    (:out result)))
 
 (defn ^:private stty-part->map [part]
   (let [[k v] (string/split part #"=")]
     {(keyword k) v}))
 
 (defn current []
-  (let [stty-str   (-> (sh "/bin/sh" "-c" "stty -g < /dev/tty") :out string/trim-newline)
+  (let [stty-str   (-> (sh! "stty -g < /dev/tty") string/trim-newline)
         stty-parts (string/split stty-str #":")
         stty-maps  (map stty-part->map stty-parts)]
     (apply merge stty-maps)))
@@ -19,15 +28,15 @@
                        (map #(mapv name %))
                        (map #(string/join "=" %))
                        (string/join ":"))]
-    (sh "/bin/sh" "-c" (str "stty " stty-args " < /dev/tty"))))
+    (sh! "stty" stty-args "< /dev/tty")))
 
 (defn set-flags! [& flags]
   (let [stty-args (->> flags (map name) (string/join " "))]
-    (sh "/bin/sh" "-c" (str "stty " stty-args " < /dev/tty"))))
+    (sh! "stty" stty-args "< /dev/tty")))
 
 (defn unset-flags! [& flags]
   (let [stty-args (->> flags
                        (map name)
                        (map #(string/replace % #"^" "-"))
                        (string/join " "))]
-    (sh "/bin/sh" "-c" (str "stty " stty-args " < /dev/tty"))))
+    (sh! "stty" stty-args "< /dev/tty")))

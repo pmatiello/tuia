970330de14af3c4dc4902b59cbdab13ab9e36279
Pedro Matiello <pedro@pmatiello.me>
2021-09-14 22:33:15 -0300

    Provide a main-loop function.
    - Consumes terminal events;
    - Invokes a `handler` function on each event;
    - Invokes a `render` function every time a given atom is modified.

--- demo/pmatiello/terminus/input_demo.clj
+++ demo/pmatiello/terminus/input_demo.clj
@@ -1,6 +1,5 @@
 (ns pmatiello.terminus.input-demo
   (:require [clojure.java.shell :refer [sh]]
-            [pmatiello.terminus.ansi.input :as input]
             [pmatiello.terminus.ansi.cursor :as cursor]
             [pmatiello.terminus.ansi.erase :as erase]
             [pmatiello.terminus.framework :as framework]
@@ -34,13 +33,6 @@
   (when (-> event :value #{:eot})
     (throw (ex-info "Interrupted" {}))))
 
-(defn mainloop
-  [handle-fn render-fn state in]
-  (add-watch state :state-changed (fn [_ _ old-state new-state]
-                                    (render-fn old-state new-state)))
-  (swap! state assoc :terminus/init true)
-  (->> in input/reader->event-seq (mapv handle-fn)))
-
 (defn -main []
   (framework/with-raw-tty
-    (mainloop handle render state *in*)))
+    (framework/with-mainloop handle render state *in*)))

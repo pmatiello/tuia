64c1e638dd3d5b1f57b4c8c039bd63c787373e30
Pedro Matiello <pedro@pmatiello.me>
2021-09-23 20:24:10 -0300

    Hide with-mainloop and with-raw-tty functions as implementation details.

--- src/pmatiello/terminus/framework.clj
+++ src/pmatiello/terminus/framework.clj
@@ -2,13 +2,7 @@
   (:require [pmatiello.terminus.internal.framework.raw-tty :as raw-tty]
             [pmatiello.terminus.internal.framework.mainloop :as mainloop]))
 
-(def ^:macro with-raw-tty
-  #'raw-tty/with-raw-tty)
-
-(def with-mainloop
-  mainloop/with-mainloop)
-
 (defn new-tty-app
   [handle-fn render-fn state input]
   (raw-tty/with-raw-tty
-    (mainloop/with-mainloop handle-fn render-fn state input)))
+    #(mainloop/with-mainloop handle-fn render-fn state input)))
--- test/pmatiello/terminus/internal/framework/raw_tty_test.clj
+++ test/pmatiello/terminus/internal/framework/raw_tty_test.clj
@@ -8,17 +8,17 @@
 
 (mfn/deftest with-raw-tty-test
   (mfn/testing "runs given body"
-    (raw-tty/with-raw-tty (func))
+    (raw-tty/with-raw-tty func)
     (mfn/verifying
       (func) nil (mockfn.matchers/exactly 1)))
 
   (mfn/testing "starts raw mode"
-    (raw-tty/with-raw-tty (func))
+    (raw-tty/with-raw-tty func)
     (mfn/verifying
       (stty/unset-flags! :icanon :echo) nil (mockfn.matchers/exactly 1)))
 
   (mfn/testing "returns to original terminal settings"
-    (raw-tty/with-raw-tty (func))
+    (raw-tty/with-raw-tty func)
     (mfn/providing
       (stty/current) 'stty-current)
     (mfn/verifying

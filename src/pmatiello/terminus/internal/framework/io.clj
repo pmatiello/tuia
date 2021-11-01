220149b8de88ef079300c4f8cce2d262bdb37efb
Pedro Matiello <pmatiello@spacelab.local>
2021-11-01 00:07:08 -0300

    Add minimal io/render function.

--- src/pmatiello/terminus/internal/framework/io.clj
+++ src/pmatiello/terminus/internal/framework/io.clj
@@ -1,5 +1,14 @@
 (ns pmatiello.terminus.internal.framework.io
-  (:require [pmatiello.terminus.internal.tty.stty :as stty]))
+  (:require [pmatiello.terminus.internal.ansi.cursor :as cursor]
+            [pmatiello.terminus.internal.tty.stty :as stty])
+  (:import (java.io Writer)))
+
+(defn render [^Writer output window buffer]
+  (let [{:keys [x y w h]} window
+        indexed-buffer (map vector (range) buffer)]
+    (doseq [[offset ^String line] indexed-buffer]
+      (.append output (str (cursor/position (+ y offset) x) line)))
+    (.flush output)))
 
 (defn with-raw-tty [func]
   (let [initial-stty (stty/current)]
--- test/pmatiello/terminus/internal/framework/io_test.clj
+++ test/pmatiello/terminus/internal/framework/io_test.clj
@@ -1,10 +1,19 @@
 (ns pmatiello.terminus.internal.framework.io-test
   (:require [clojure.test :refer :all]
             [mockfn.clj-test :as mfn]
+            [pmatiello.terminus.internal.ansi.cursor :as cursor]
             [pmatiello.terminus.internal.framework.io :as io]
-            [pmatiello.terminus.internal.tty.stty :as stty]))
+            [pmatiello.terminus.internal.tty.stty :as stty])
+  (:import (java.io StringWriter)))
 
 (defn func [])
+
+(mfn/deftest render-test
+  (let [output (StringWriter.)]
+    (io/render output {:x 3 :y 4 :w 5 :h 2} ["line1" "line2"])
+    (is (= (str (cursor/position 4 3) "line1"
+                (cursor/position 5 3) "line2")
+           (str output)))))
 
 (mfn/deftest with-raw-tty-test
   (mfn/testing "runs given body"

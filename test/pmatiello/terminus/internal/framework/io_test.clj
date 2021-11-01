2866b5776c041768048e13d7090e15866221be88
Pedro Matiello <pedro@pmatiello.me>
2021-11-01 00:07:08 -0300

    Make io/render update the entire window, filling with blank space as necessary.

--- test/pmatiello/terminus/internal/framework/io_test.clj
+++ test/pmatiello/terminus/internal/framework/io_test.clj
@@ -31,6 +31,21 @@
       (io/render output {:x 3 :y 4 :w 5 :h 2} ["line1-ignored" "line2-ignored"])
       (is (= (str (cursor/position 4 3) "line1"
                   (cursor/position 5 3) "line2")
+             (str output)))))
+
+  (testing "fills missing width in buffer with blank space"
+    (let [output (new-writer)]
+      (io/render output {:x 3 :y 4 :w 8 :h 2} ["line1" "line2"])
+      (is (= (str (cursor/position 4 3) "line1   "
+                  (cursor/position 5 3) "line2   ")
+             (str output)))))
+
+  (testing "fills missing height in buffer with blank space"
+    (let [output (new-writer)]
+      (io/render output {:x 3 :y 4 :w 5 :h 3} ["line1" "line2"])
+      (is (= (str (cursor/position 4 3) "line1"
+                  (cursor/position 5 3) "line2"
+                  (cursor/position 6 3) "     ")
              (str output))))))
 
 (mfn/deftest with-raw-tty-test

e048bd8d6aba109ecd1e90a19a2c93092d166e01
Pedro Matiello <pedro@pmatiello.me>
2021-11-01 21:38:11 -0300

    Synchronize writes using io/render.

--- src/pmatiello/terminus/internal/framework/io.clj
+++ src/pmatiello/terminus/internal/framework/io.clj
@@ -18,12 +18,13 @@
        (map #(cropped-width width %))))
 
 (defn render [^Writer output window buffer]
-  (let [{:keys [x y w h]} window
-        cropped-buf (cropped buffer w h)
-        indexed-buf (map vector (range) cropped-buf)]
-    (doseq [[offset ^String line] indexed-buf]
-      (.append output (str (cursor/position (+ y offset) x) line)))
-    (.flush output)))
+  (locking output
+    (let [{:keys [x y w h]} window
+          cropped-buf (cropped buffer w h)
+          indexed-buf (map vector (range) cropped-buf)]
+      (doseq [[offset ^String line] indexed-buf]
+        (.append output (str (cursor/position (+ y offset) x) line)))
+      (.flush output))))
 
 (defn with-raw-tty [func]
   (let [initial-stty (stty/current)]

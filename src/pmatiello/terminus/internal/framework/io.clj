686c77da7117ad9be1d7a5c2a0fc4cdf23bce498
Pedro Matiello <pedro@pmatiello.me>
2021-11-01 22:30:31 -0300

    Add io/clear-screen! function to clear the screen.

--- src/pmatiello/terminus/internal/framework/io.clj
+++ src/pmatiello/terminus/internal/framework/io.clj
@@ -1,5 +1,6 @@
 (ns pmatiello.terminus.internal.framework.io
   (:require [pmatiello.terminus.internal.ansi.cursor :as cursor]
+            [pmatiello.terminus.internal.ansi.erase :as erase]
             [pmatiello.terminus.internal.tty.stty :as stty])
   (:import (java.io Writer)))
 
@@ -25,6 +26,10 @@
       (doseq [[offset ^String line] indexed-buf]
         (.append output (str (cursor/position (+ y offset) x) line)))
       (.flush output))))
+
+(defn clear-screen! [^Writer output]
+  (.append output (str erase/all (cursor/position 1 1)))
+  (.flush output))
 
 (defn with-raw-tty [func]
   (let [initial-stty (stty/current)]

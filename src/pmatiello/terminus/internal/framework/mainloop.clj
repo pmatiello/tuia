241470b69dd0c60ca6fe9df5b72f3a176cf4ae54
Pedro Matiello <pedro@pmatiello.me>
2021-11-07 15:52:54 -0300

    Make *out* unavailable during framework/new-tty-app execution.

--- src/pmatiello/terminus/internal/framework/mainloop.clj
+++ src/pmatiello/terminus/internal/framework/mainloop.clj
@@ -1,10 +1,10 @@
 (ns pmatiello.terminus.internal.framework.mainloop)
 
 (defn with-mainloop
-  [handle-fn render-fn state input]
+  [handle-fn render-fn state input output]
   (try
     (add-watch state ::state-changed (fn [_ _ old-state new-state]
-                                       (render-fn old-state new-state)))
+                                       (render-fn output old-state new-state)))
     (swap! state assoc ::init true)
     (mapv handle-fn input)
     (finally

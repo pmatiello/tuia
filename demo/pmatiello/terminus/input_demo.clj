3eeabbb1600f4dd52d777ba65bf06c9d23e1b86d
Pedro Matiello <pedro@pmatiello.me>
2021-11-12 21:06:55 -0300

    Exposed several internal io funtions in tty.io.

--- demo/pmatiello/terminus/input_demo.clj
+++ demo/pmatiello/terminus/input_demo.clj
@@ -1,9 +1,9 @@
 (ns pmatiello.terminus.input-demo
   (:require [pmatiello.terminus.internal.ansi.cursor :as cursor]
             [pmatiello.terminus.core :as tty]
-            [pmatiello.terminus.internal.io :as io]
-            [pmatiello.terminus.internal.mainloop :as mainloop]
-            [clojure.string :as str])
+            [pmatiello.terminus.io :as tty.io]
+            [pmatiello.terminus.internal.io :as tty.internal.io]
+            [pmatiello.terminus.internal.mainloop :as mainloop])
   (:import (clojure.lang ExceptionInfo)))
 
 (def state
@@ -17,14 +17,14 @@
 
 (defn render [output old-state new-state]
   (when-not (::mainloop/init old-state)
-    (io/clear-screen! output)
-    (io/print! output header {:x 1 :y 1 :w 23 :h 3}))
+    (tty.io/clear-screen! output)
+    (tty.io/print! output header {:x 1 :y 1 :w 23 :h 3}))
 
   (when (not= (:curr-pos? old-state) (:curr-pos? new-state))
-    (io/print! output [cursor/current-position] {:x 1 :y 10 :w 4 :h 5}))
+    (tty.io/print! output [cursor/current-position] {:x 1 :y 10 :w 4 :h 5}))
 
-  (io/print! output (:events new-state) {:x 1 :y 5 :w 40 :h 6})
-  (io/place-cursor! output {:x 1 :y 10}))
+  (tty.io/print! output (:events new-state) {:x 1 :y 5 :w 40 :h 6})
+  (tty.io/place-cursor! output {:x 1 :y 10}))
 
 (defn handle [event]
   (swap! state assoc :events
@@ -38,10 +38,10 @@
 
 (defn -main []
   (try
-    (->> (atom []) io/hide-cursor! (io/write! *out*))
+    (->> (atom []) tty.io/hide-cursor! (tty.internal.io/write! *out*))
     (tty/new-tty-app handle render state)
     (catch ExceptionInfo ex
-      (->> (atom []) io/show-cursor! (io/write! *out*))
+      (->> (atom []) tty.io/show-cursor! (tty.internal.io/write! *out*))
       (if (-> ex ex-data :cause #{:interrupted})
         (System/exit 0)
         (throw ex)))))

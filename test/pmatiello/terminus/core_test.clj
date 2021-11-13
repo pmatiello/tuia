5deb30195ce827b971265ccffd1c85f2c4d0b7b3
Pedro Matiello <pedro@pmatiello.me>
2021-11-12 21:06:55 -0300

    Exposed several internal io funtions in tty.io.

--- test/pmatiello/terminus/core_test.clj
+++
@@ -1,3 +0,0 @@
-(ns pmatiello.terminus.core-test
-  (:require [clojure.test :refer :all]
-            [pmatiello.terminus.core :as tty]))
--- test/pmatiello/terminus/internal/io_test.clj
+++ test/pmatiello/terminus/internal/io_test.clj
@@ -1,21 +1,12 @@
 (ns pmatiello.terminus.internal.io-test
   (:require [clojure.test :refer :all]
             [mockfn.clj-test :as mfn]
-            [pmatiello.terminus.internal.ansi.cursor :as cursor]
-            [pmatiello.terminus.internal.ansi.erase :as erase]
             [pmatiello.terminus.internal.io :as io]
-            [pmatiello.terminus.internal.stty :as stty]
-            [pmatiello.terminus.internal.fixtures :as fixtures]
-            [clojure.string :as string])
+            [pmatiello.terminus.internal.stty :as stty])
   (:import (java.io StringWriter)))
-
-(use-fixtures :each fixtures/with-readable-csi)
 
 (defn- new-writer []
   (StringWriter.))
-
-(defn- new-output []
-  (atom []))
 
 (defn func [])
 
@@ -24,72 +15,6 @@
     (let [writer (new-writer)]
       (io/write! writer [":str1" ":str2"])
       (is (= ":str1:str2" (str writer))))))
-
-(deftest print!-test
-  (testing "prints buffer at location"
-    (let [output (new-output)]
-      (io/print! output ["line1" "line2"] {:x 3 :y 4 :w 5 :h 2})
-      (is (= [(str (cursor/position 4 3) "line1")
-              (str (cursor/position 5 3) "line2")]
-             @output))))
-
-  (testing "prints only the given height"
-    (let [output (new-output)]
-      (io/print! output ["line1" "line2" "ignored"] {:x 3 :y 4 :w 5 :h 2})
-      (is (= [(str (cursor/position 4 3) "line1")
-              (str (cursor/position 5 3) "line2")]
-             @output))))
-
-  (testing "prints only the given width"
-    (let [output (new-output)]
-      (io/print! output ["line1-ignored" "line2-ignored"] {:x 3 :y 4 :w 5 :h 2})
-      (is (= [(str (cursor/position 4 3) "line1")
-              (str (cursor/position 5 3) "line2")]
-             @output))))
-
-  (testing "fills missing width in buffer with blank space"
-    (let [output (new-output)]
-      (io/print! output ["line1" "line2"] {:x 3 :y 4 :w 8 :h 2})
-      (is (= [(str (cursor/position 4 3) "line1   ")
-              (str (cursor/position 5 3) "line2   ")]
-             @output))))
-
-  (testing "fills missing height in buffer with blank space"
-    (let [output (new-output)]
-      (io/print! output ["line1" "line2"] {:x 3 :y 4 :w 5 :h 3})
-      (is (= [(str (cursor/position 4 3) "line1")
-              (str (cursor/position 5 3) "line2")
-              (str (cursor/position 6 3) "     ")]
-             @output)))))
-
-(deftest clear-screen!-test
-  (testing "clears screen"
-    (let [output (new-output)]
-      (io/clear-screen! output)
-      (is (some #{erase/all} @output))))
-
-  (testing "moves cursor to top left"
-    (let [output (new-output)]
-      (io/clear-screen! output)
-      (is (some #{(cursor/position 1 1)} @output)))))
-
-(deftest show-cursor!-test
-  (testing "shows input cursor"
-    (let [output (new-output)]
-      (io/show-cursor! output)
-      (is (= [cursor/show] @output)))))
-
-(deftest hide-cursor!-test
-  (testing "hide input cursor"
-    (let [output (new-output)]
-      (io/hide-cursor! output)
-      (is (= [cursor/hide] @output)))))
-
-(deftest place-cursor!-test
-  (testing "moves cursor to given position"
-    (let [output (new-output)]
-      (io/place-cursor! output {:x 10 :y 5})
-      (is (= [(cursor/position 5 10)] @output)))))
 
 (mfn/deftest with-raw-tty-test
   (mfn/testing "runs given body"

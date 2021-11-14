965c4f5ecad4bf616845949e2aac3d41c0b879b1
Pedro Matiello <pedro@pmatiello.me>
2021-11-13 22:58:33 -0300

    Ensure that render function is called on both initialization and termination.

--- test/pmatiello/tty/internal/mainloop_test.clj
+++ test/pmatiello/tty/internal/mainloop_test.clj
@@ -1,27 +1,33 @@
 (ns pmatiello.tty.internal.mainloop-test
   (:require [clojure.test :refer :all]
             [pmatiello.tty.internal.mainloop :as mainloop]
+            [pmatiello.tty :as tty]
             [mockfn.clj-test :as mfn]
-            [mockfn.matchers :as mfn.matchers])
+            [mockfn.matchers :as mfn.matchers]
+            [clojure.set :as set])
   (:import (clojure.lang Atom)))
 
 (declare handle-fn)
 (declare render-fn)
 
 (def output-buffer
-  (reify mfn.matchers/Matcher
-    (matches? [this actual]
-      (and (instance? Atom actual)
-           (vector? @actual)
-           (empty? @actual)))
-    (description [this] "output buffer")))
+  (mfn.matchers/pred
+    #(and (instance? Atom %) (vector? @%) (empty? @%))))
+
+(def anything?
+  mfn.matchers/any)
+
+(defn embeds? [submap]
+  (mfn.matchers/pred
+    #(set/subset? (set submap) (set %))))
 
 (mfn/deftest with-mainloop-test
-  (mfn/testing "calls render function on initialisation"
+  (mfn/testing "calls render function on initialisation and termination"
     (let [state (atom {})]
       (mainloop/with-mainloop handle-fn render-fn state [] 'output!))
     (mfn/verifying
-      (render-fn output-buffer {} {::mainloop/init true}) 'irrelevant (mfn.matchers/exactly 1)))
+      (render-fn output-buffer {} {::tty/init true}) 'irrelevant (mfn.matchers/exactly 1)
+      (render-fn output-buffer (anything?) (embeds? {::tty/halt true})) 'irrelevant (mfn.matchers/exactly 1)))
 
   (mfn/testing "invokes the handler function for each event in the input reader"
     (let [state (atom {})]
@@ -30,20 +36,22 @@
       (handle-fn 'ev1) nil (mfn.matchers/exactly 1)
       (handle-fn 'ev2) nil (mfn.matchers/exactly 1))
     (mfn/providing
-      (render-fn (mfn.matchers/any) (mfn.matchers/any) (mfn.matchers/any)) 'irrelevant))
+      (render-fn (anything?) (anything?) (anything?)) 'irrelevant))
 
   (mfn/testing "invokes the render function on each change to the state atom"
     (let [state (atom {})
           handle-fn (fn [event] (swap! state assoc :last-event event))]
       (mainloop/with-mainloop handle-fn render-fn state ['ev1 'ev2] 'output!))
     (mfn/verifying
-      (render-fn output-buffer (mfn.matchers/any) {::mainloop/init true}) 'irrelevant (mfn.matchers/any)
-      (render-fn output-buffer (mfn.matchers/any) {::mainloop/init true :last-event 'ev1}) nil (mfn.matchers/exactly 1)
-      (render-fn output-buffer (mfn.matchers/any) {::mainloop/init true :last-event 'ev2}) nil (mfn.matchers/exactly 1)))
+      (render-fn output-buffer (anything?) {::tty/init true}) 'irrelevant (anything?)
+      (render-fn output-buffer (anything?) (embeds? {::tty/halt true})) 'irrelevant (anything?)
+      (render-fn output-buffer (anything?) (embeds? {:last-event 'ev1})) nil (mfn.matchers/exactly 1)
+      (render-fn output-buffer (anything?) (embeds? {:last-event 'ev2})) nil (mfn.matchers/exactly 1)))
 
   (mfn/testing "no longer invokes render function when mainloop is over"
     (let [state (atom {})]
       (mainloop/with-mainloop handle-fn render-fn state [] 'output!)
       (swap! state assoc :x :y))
     (mfn/verifying
-      (render-fn output-buffer {} {::mainloop/init true}) 'irrelevant (mfn.matchers/any))))
+      (render-fn output-buffer (anything?) {::tty/init true}) 'irrelevant (anything?)
+      (render-fn output-buffer (anything?) {::tty/init true ::tty/halt true}) 'irrelevant (anything?))))

c5ce0c43b6319108f080a2a106e8cbc2a00ecd94
Pedro Matiello <pedro@pmatiello.me>
2021-11-14 22:27:51 -0300

    Ensure that handler function is called on both initialization and termination.

--- src/pmatiello/tty/internal/mainloop.clj
+++ src/pmatiello/tty/internal/mainloop.clj
@@ -6,18 +6,23 @@
       (render-fn output old-state new-state)
       (output! @output))))
 
-(defn- notify! [state event]
+(defn- call-sync! [handle-fn event]
+  (locking handle-fn
+    (handle-fn event)))
+
+(defn- notify! [handle-fn state event]
+  (call-sync! handle-fn {:event event :value true})
   (swap! state assoc event true))
 
 (defn with-mainloop
   [handle-fn render-fn state input output!]
   (try
     (add-watch state ::state-changed (watch-fn render-fn output!))
-    (notify! state :pmatiello.tty/init)
+    (notify! handle-fn state :pmatiello.tty/init)
 
     (doseq [event input]
-      (handle-fn event))
+      (call-sync! handle-fn event))
 
     (finally
-      (notify! state :pmatiello.tty/halt)
+      (notify! handle-fn state :pmatiello.tty/halt)
       (remove-watch state ::state-changed))))

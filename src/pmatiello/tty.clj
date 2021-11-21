57c1612a5b0bdfc1e3d1ee512914b1d712ecb453
Pedro Matiello <pedro@pmatiello.me>
2021-11-21 20:23:15 -0300

    Add docstrings and specs to functions in the tty namespace.

--- src/pmatiello/tty.clj
+++ src/pmatiello/tty.clj
@@ -1,10 +1,27 @@
 (ns pmatiello.tty
   (:require [pmatiello.tty.internal.ansi.input :as input]
             [pmatiello.tty.internal.io :as io]
-            [pmatiello.tty.internal.mainloop :as mainloop])
-  (:import (java.io Writer)))
+            [pmatiello.tty.internal.mainloop :as mainloop]
+            [clojure.spec.alpha :as s])
+  (:import (java.io Writer)
+           (clojure.lang Atom)))
+
+(s/def ::state
+  (s/and #(instance? Atom %)
+         #(-> % deref map?)))
 
 (defn init!
+  "Initializes the application main loop.
+
+  handle-fn: function invoked for each input event. Args:
+    - event
+
+  render-fn: function invoked for each change in state. Args:
+    - output: a #::tty.io/output object
+    - old-state: previous ::state
+    - new-state: updated ::state
+
+  state: mutable application state"
   [handle-fn render-fn state]
   (let [input (input/reader->event-seq *in*)
         output! (partial io/write! *out*)]
@@ -12,3 +29,6 @@
       [*out* (Writer/nullWriter)]
       (io/with-raw-tty
         #(mainloop/with-mainloop handle-fn render-fn state input output!)))))
+
+(s/fdef init!
+  :args (s/cat :handle-fn fn? :render-fn fn? :state ::state))

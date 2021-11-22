406e16f3027f027ad099600a557f49da0408873d
Pedro Matiello <pedro@pmatiello.me>
2021-11-22 00:09:43 -0300

    Add docstrings and specs to functions in the tty.internal.signal namespace.

--- test/pmatiello/tty/internal/signal_test.clj
+++ test/pmatiello/tty/internal/signal_test.clj
@@ -1,7 +1,10 @@
 (ns pmatiello.tty.internal.signal-test
   (:require [clojure.test :refer :all]
-            [pmatiello.tty.internal.signal :as signal])
+            [pmatiello.tty.internal.signal :as signal]
+            [pmatiello.tty.internal.fixtures :as fixtures])
   (:import (sun.misc Signal)))
+
+(use-fixtures :each fixtures/with-spec-instrumentation)
 
 (deftest trap-test
   (testing "executes the given function when the signal is raised"
